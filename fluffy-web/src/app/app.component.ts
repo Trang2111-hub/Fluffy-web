d7fd0cb71e62e0a8c1695afd8d905d6c2e9abe34
TrangVo <vothihuyentrang2816@gmail.com>
2025-03-12 00:45:29 +0700

    Tạo component product-detail

--- fluffy-web/src/app/app.component.ts
+++ fluffy-web/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
+import { ProductDetailComponent } from "./pages/product-detail/product-detail.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, ProductDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- fluffy-web/src/app/pages/product-detail/product-detail.component.ts
+++ fluffy-web/src/app/pages/product-detail/product-detail.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  quantity: number = 1;
 
+  increaseQuantity() {
+    this.quantity++;
+  }
+
+  decreaseQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
 }
