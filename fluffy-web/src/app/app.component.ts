7667b9158617eaa172e23360a060f6244d0f0e60
ThuyTrang <trangttt22411@st.uel.edu.vn>
2025-03-13 22:50:14 +0700

    Trang dang ky

--- fluffy-web/src/app/app.component.ts
+++ fluffy-web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,LoginComponent],
+  imports: [RouterOutlet,LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
