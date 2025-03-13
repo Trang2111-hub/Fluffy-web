9a77a9e98ef27f596eb0e72e304be06ff1aa2f30
ThuyTrang <trangttt22411@st.uel.edu.vn>
2025-03-13 21:14:41 +0700

    Trang dang nhap

--- fluffy-web/src/app/pages/login/login.component.ts
+++ fluffy-web/src/app/pages/login/login.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
+  showPassword: boolean = false;
 
+  // Toggle hiển thị mật khẩu
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  // Xử lý đăng nhập
+  onLogin(event: Event) {
+    event.preventDefault(); // Ngừng hành động mặc định của form
+
+    // Kiểm tra đăng nhập giả lập
+    if (this.email === 'admin@example.com' && this.password === '123456') {
+      alert('Đăng nhập thành công!');
+    } else {
+      alert('Email hoặc mật khẩu không đúng, vui lòng thử lại!');
+    }
+  }
 }
