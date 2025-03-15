89c727398d9594569ce03aead4688fcd17315735
minhhieunguyenn <hieuntm22411@st.uel.edu.vn>
2025-03-15 10:20:49 +0700

    code trang thông tin cá nhân

--- fluffy-web/src/app/app.component.ts
+++ fluffy-web/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AccountSettingsComponent } from './pages/account-settings/account-settings.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePageComponent, CommonModule, ReactiveFormsModule, AccountSettingsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
