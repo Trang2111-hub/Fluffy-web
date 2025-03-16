23fb37b359909c0150f4cbd81885e6552916f2dc
hlnetr21354 <nqhl21354@gmail.com>
2025-03-17 00:54:24 +0700

    chatbox

--- fluffy-web/src/app/app.component.ts
+++ fluffy-web/src/app/app.component.ts
@@ -10,11 +10,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AccountSettingsComponent } from './pages/account-settings/account-settings.component';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, ProductDetailComponent, LoginComponent, SignupComponent, PaymentComponent, HomePageComponent, CommonModule, ReactiveFormsModule, AccountSettingsComponent],
+  imports: [RouterOutlet, FooterComponent, ProductDetailComponent, LoginComponent, SignupComponent, PaymentComponent, HomePageComponent, CommonModule, ReactiveFormsModule, AccountSettingsComponent, ProductPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
