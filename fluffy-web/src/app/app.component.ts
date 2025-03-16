76bfc3501ae8b60f3b8ae7e88c02324764c7bfe5
minhhieunguyenn <hieuntm22411@st.uel.edu.vn>
2025-03-16 08:25:45 +0700

    Homepage

--- fluffy-web/src/app/app.component.ts
+++ fluffy-web/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './pages/homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
