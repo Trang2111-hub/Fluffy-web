17613ce9c63661a028c908b169b82443cc979fd4
ThuyTrang <trangttt22411@st.uel.edu.vn>
2025-03-20 21:21:26 +0700

    Bo suu tap

--- fluffy-web/src/app/app.component.ts
+++ fluffy-web/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { PaymentComponent } from './pages/payment/payment.component';
+import { CollectionComponent } from './pages/collection/collection.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,LoginComponent, SignupComponent, PaymentComponent],
+  imports: [RouterOutlet,LoginComponent, SignupComponent, PaymentComponent, CollectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
