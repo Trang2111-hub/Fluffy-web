73e7b0786a76811b4037d4bb6b0f49d8d17e74b0
ThuyTrang <trangttt22411@st.uel.edu.vn>
2025-03-21 11:48:45 +0700

    Chinh sach ban hang

---
+++ fluffy-web/src/app/pages/salespolicy/salespolicy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salespolicy',
+  imports: [],
+  templateUrl: './salespolicy.component.html',
+  styleUrl: './salespolicy.component.css'
+})
+export class SalespolicyComponent {
+
+}
