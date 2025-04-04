dfaff52e2a49855d43f5dee9da3e7d7c1984d81c
ThuyTrang <trangttt22411@st.uel.edu.vn>
2025-04-04 20:38:45 +0700

    Chinh sach ban hang

--- fluffy-web/src/app/pages/salespolicy/salespolicy.component.ts
+++ fluffy-web/src/app/pages/salespolicy/salespolicy.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-salespolicy',
   standalone: true,
-  imports: [],
   templateUrl: './salespolicy.component.html',
-  styleUrl: './salespolicy.component.css'
+  styleUrl: './salespolicy.component.css',
+  imports: [CommonModule] 
 })
-export class SalespolicyComponent {
+export class SalespolicyComponent implements OnInit{
+  policies = [
+    { id: 'policy-1', title: 'Chính sách đổi trả và bảo hành' },
+    { id: 'policy-2', title: 'Chính sách bảo mật' },
+    { id: 'policy-3', title: 'Chính sách vận chuyển' },
+    { id: 'policy-4', title: 'Chính sách thanh toán' }
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      window.scrollTo({
+        top: element.offsetTop - 20,
+        behavior: 'smooth'
+      });
+    }
+  }
 }
