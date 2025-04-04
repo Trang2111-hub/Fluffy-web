ee7c01ad9d16ea9706473e9e2cf78f8645856e17
FacultyTrinh <khoattd22411@st.uel.edu.vn>
2025-04-04 20:47:33 +0700

    Cập nhật giỏ hàng sản phẩm dẫn thực từ trang giỏ hàng và product-card khi bấm mua ngay

--- fluffy-web/src/app/pages/payment/payment.component.ts
+++ fluffy-web/src/app/pages/payment/payment.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CartService } from '../../../services/cart.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
@@ -9,8 +11,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css']
 })
-export class PaymentComponent {
-  // Cập nhật đối tượng orderDetails với tất cả các trường cần thiết
+export class PaymentComponent implements OnInit {
   orderDetails = {
     name: '',
     email: '',
@@ -25,16 +26,52 @@ export class PaymentComponent {
     paymentMethod: ''
   };
 
+  checkoutProducts: any[] = [];
+  totalAmount = 0;
+
+  constructor(
+    private cartService: CartService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    // Flow 1: Lấy sản phẩm từ giỏ hàng
+    this.cartService.getCheckoutProducts().subscribe(products => {
+      this.checkoutProducts = products;
+      this.calculateTotal();
+    });
+
+    // Flow 2: Lấy sản phẩm từ trang sản phẩm
+    this.route.queryParams.subscribe(params => {
+      if (params['product']) {
+        const product = JSON.parse(params['product']);
+        this.checkoutProducts = [product];
+        this.calculateTotal();
+      }
+    });
+  }
+
+  calculateTotal() {
+    this.totalAmount = this.checkoutProducts.reduce((total, product) => {
+      return total + product.totalPrice;
+    }, 0);
+  }
+
   onSubmit() {
     if (this.isFormValid()) {
       alert('Đơn hàng đã được đặt thành công!');
-      // Thực hiện các thao tác như gửi thông tin qua API hoặc các xử lý khác.
     } else {
       alert('Vui lòng điền đầy đủ thông tin!');
     }
   }
 
   isFormValid() {
-    return this.orderDetails.name && this.orderDetails.email && this.orderDetails.phone && this.orderDetails.city && this.orderDetails.district && this.orderDetails.ward;
+    return this.orderDetails.name && this.orderDetails.email && this.orderDetails.phone && 
+           this.orderDetails.city && this.orderDetails.district && this.orderDetails.ward;
+  }
+
+  // Thêm để xử lý sự kiện "Chọn mã"
+  applyPromo() {
+    alert('Chức năng chọn mã khuyến mãi đang được phát triển!');
   }
 }
